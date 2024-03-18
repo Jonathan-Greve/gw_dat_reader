d826204c6a434b339b99dc213b678696cefc41c5
Jonathan Bjørn Greve <jonagrev@gmail.com>
2024-03-19 02:33:37 +0800

    test cross compilation stuff

--- xmake.lua
+++ xmake.lua
@@ -18,6 +18,9 @@ function add_platform_specific_flags()
 end
 
 add_requires("flatbuffers")
+if is_cross() then -- requires xmake 2.8.8
+    add_requires("flatbuffers~host", { host = true })
+end
 
 target("gw_dat_reader")
     set_kind("binary")
@@ -31,6 +34,9 @@ target_end()
 add_requires("gtest", {configs = {main = false, gmock = true}})
 add_requires("spdlog")
 add_requires("flatbuffers")
+if is_cross() then -- requires xmake 2.8.8
+    add_requires("flatbuffers~host", { host = true })
+end
 
 -- Iterate over test files and create test targets
 for _, file in ipairs(os.files("tests/test_*.cpp")) do
@@ -47,6 +53,9 @@ end
 -- Run flatc command to generate C++ code from .fbs files
 local function get_flatc_cmd(target)
     local flatbuffers_installdir = target:pkg("flatbuffers"):installdir()
+    local flatc_exe = is_plat("windows") and "flatc.exe" or "flatc"
+    local flatc_path = path.join(flatbuffers_installdir, "bin", flatc_exe)
+
     local fbs_dir = path.join(os.scriptdir(), "flatbuffers", "schemes")
     local fbs_files = os.files(path.join(fbs_dir, "*.fbs"))
     
