a2046ba38101e4a61a50bcc6c0820c384363edf6
Jonathan Bjørn Greve <jonagrev@gmail.com>
2024-03-19 01:06:27 +0800

    Move before_build to end of file until I fix the target name extraction step in the workflow files

--- xmake.lua
+++ xmake.lua
@@ -19,32 +19,6 @@ end
 
 add_requires("flatbuffers")
 
--- Run flatc command to generate C++ code from .fbs files
-local function get_flatc_cmd(target)
-    local flatbuffers_installdir = target:pkg("flatbuffers"):installdir()
-    local fbs_dir = path.join(os.scriptdir(), "flatbuffers", "schemes")
-    local fbs_files = os.files(path.join(fbs_dir, "*.fbs"))
-    
-    local flatc_exe = is_plat("windows") and "flatc.exe" or "flatc"
-    local flatc_path = path.join(flatbuffers_installdir, "bin", flatc_exe)
-    
-    local output_dir = path.join(os.scriptdir(), "flatbuffers", "output")
-    
-    local flatc_cmd = { flatc_path, "--cpp", "--grpc", "-o", output_dir }
-    for _, fbs_file in ipairs(fbs_files) do
-        table.insert(flatc_cmd, fbs_file)
-    end
-    
-    -- Return as a string
-    return table.concat(flatc_cmd, " ")
-end
-
-before_build(function (target)
-    local flatc_cmd = get_flatc_cmd(target)
-    --print("Running flatc command: " .. flatc_cmd, " ")
-    os.run(flatc_cmd)
-end)
-
 target("gw_dat_reader")
     set_kind("binary")
     add_files("src/*.cpp")
@@ -68,4 +42,30 @@ for _, file in ipairs(os.files("tests/test_*.cpp")) do
         add_files(file)
         add_tests("default")
         add_platform_specific_flags()
+end
+
+before_build(function (target)
+    local flatc_cmd = get_flatc_cmd(target)
+    --print("Running flatc command: " .. flatc_cmd, " ")
+    os.run(flatc_cmd)
+end)
+
+-- Run flatc command to generate C++ code from .fbs files
+local function get_flatc_cmd(target)
+    local flatbuffers_installdir = target:pkg("flatbuffers"):installdir()
+    local fbs_dir = path.join(os.scriptdir(), "flatbuffers", "schemes")
+    local fbs_files = os.files(path.join(fbs_dir, "*.fbs"))
+    
+    local flatc_exe = is_plat("windows") and "flatc.exe" or "flatc"
+    local flatc_path = path.join(flatbuffers_installdir, "bin", flatc_exe)
+    
+    local output_dir = path.join(os.scriptdir(), "flatbuffers", "output")
+    
+    local flatc_cmd = { flatc_path, "--cpp", "--grpc", "-o", output_dir }
+    for _, fbs_file in ipairs(fbs_files) do
+        table.insert(flatc_cmd, fbs_file)
+    end
+    
+    -- Return as a string
+    return table.concat(flatc_cmd, " ")
 end
